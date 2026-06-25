269eb1635318849e97aec67b4ae5325e2feed169
wokoside <h3dqz.i@gmail.com>
2026-06-25 19:24:50 +0300

    revised input reader class

--- src/reader/InputReader.java
+++ src/reader/InputReader.java
@@ -1,19 +1,19 @@
 package reader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class InputReader {
 
-    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    private final BufferedReader reader;
 
-    public static String readLineWithMessage (String message) {
-        System.out.println(message);
+    public InputReader(InputStream is) {
+        reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+    }
+
+    public String readLine() {
         try {
-            String line = reader.readLine();
-            return line != null ? line.trim() : "";
+            return reader.readLine();
         } catch (IOException e) {
             throw new RuntimeException("Failed to read input", e);
         }
